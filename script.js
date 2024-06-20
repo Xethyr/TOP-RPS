0b151459563b499e916ba3b18b183cfe1bc6746c
Xethyr <monteiv@me.com>
2024-06-20 16:19:30 -0500

    Finalized functionality with UI.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
+const humanScoreDiv = document.getElementById('human-score');
+const computerScoreDiv = document.getElementById('computer-score');
+const winnerDiv = document.getElementById('winner');
 
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3)
@@ -23,6 +26,7 @@ function getHumanChoice() {
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
+    let roundsPlayed = 0;
 
     function playRound(humanChoice, computerChoice) {
         const loseMessage = `You lose! ${computerChoice} beats ${humanChoice}`;
@@ -30,6 +34,7 @@ function playGame() {
 
         if (humanChoice === computerChoice) {
         console.log("It's a tie!");
+        roundsPlayed++;
         return;
         }
 
@@ -63,6 +68,22 @@ function playGame() {
                 console.log(winMessage, `Scores: You: ${humanScore}, Computer: ${computerScore}`);
             }
         }
+        roundsPlayed++;
+
+        humanScoreDiv.innerText = `Your Score: ${humanScore}`;
+        computerScoreDiv.innerText = `Computer Score: ${computerScore}`
+        if (roundsPlayed === 5) {
+            if (humanScore > computerScore) {
+                winnerDiv.innerText = 'Winner: You!';
+            }
+            if (computerScore > humanScore) {
+                winnerDiv.innerText = 'Winner: Computer!';
+            }
+            if (humanScore === computerScore) {
+                winnerDiv.innerText = "It's a tie!";
+            }
+        }
+
     }
     rockBtn.addEventListener('click', () => {
         playRound('rock', getComputerChoice());
@@ -73,6 +94,7 @@ function playGame() {
     scissorsBtn.addEventListener('click', () => {
         playRound('scissors', getComputerChoice());
     })
+
 
 }
 playGame();
